16f7a99edacc0fe43229272a72b6911a9c33611f
gryatka <edameow1@gmail.com>
2024-01-04 13:22:35 +0500

    init module as app

---
+++ src/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ActiveUser;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
